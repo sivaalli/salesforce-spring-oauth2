c9722773d68abd20c556017a1374f8948a4399d6
salli <salli@salesforce.com>
2019-05-09 13:40:31 -0400

    using session backed repository which interun uses redis backed sessions

--- src/main/java/com/example/demo/SpringSessionApplication.java
+++ src/main/java/com/example/demo/SpringSessionApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -19,19 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SpringSessionApplication{
 
-    @RequestMapping("/hi")
-    public String email(@RegisteredOAuth2AuthorizedClient("salesforce") OAuth2AuthorizedClient authorizedClient, HttpSession session) {
-        if (session != null && !session.isNew()){
-            log.info("Dyno that received request is {} and session id is {}", System.getenv("DYNO"), session.getId());
-        }else {
-            log.info("Dyno that received request is {}", System.getenv("DYNO"));
-        }
-        return "Hello " + authorizedClient.getPrincipalName() + " your access token is " + authorizedClient.getAccessToken().getTokenValue();
+    @RequestMapping("/")
+    public String email1(@RegisteredOAuth2AuthorizedClient("salesforce") OAuth2AuthorizedClient authorizedClient) {
+        log.info("Hi {}, your token is {}", authorizedClient.getPrincipalName(), authorizedClient.getAccessToken().getTokenValue());
+        return "Hi";
     }
 
-    @RequestMapping("/")
-    public String email1() {
-        return "Hi";
+    @Bean
+    public OAuth2AuthorizedClientRepository repository(){
+        return new HttpSessionOAuth2AuthorizedClientRepository();
     }
 
     @Bean
