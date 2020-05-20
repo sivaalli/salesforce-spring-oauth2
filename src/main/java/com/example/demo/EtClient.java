7b7475879d8721b26e07acb670047455316408e5
salliEG <sallievergage@gmail.com>
2020-05-20 10:37:14 -0400

    adding get permissions

---
+++ src/main/java/com/example/demo/EtClient.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2010-2020 Evergage, Inc.
+ * All rights reserved.
+ */
+
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class EtClient {
+
+    @Autowired
+    private RestTemplate rest;
+
+    public List<Item> permissions(String authToken) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authToken);
+
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        final ResponseEntity<McResponse> exchange = rest.exchange(
+                "/platform-internal/v1/users/@current/permissions?$pageSize=1000",
+                HttpMethod.GET,
+                entity,
+                McResponse.class);
+
+        return Collections.unmodifiableList(Objects.requireNonNull(exchange.getBody()).items);
+
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class McResponse {
+        public int count;
+        public int page;
+        public int pageSize;
+        public ObjectNode links;
+        public List<Item> items;
+    }
+
+    public static class Item {
+        public boolean hasAccess;
+        public String objectTypeName;
+        public String operationName;
+        public String name;
+        public int id;
+        public int mIds;
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "hasAccess=" + hasAccess +
+                    ", objectTypeName='" + objectTypeName + '\'' +
+                    ", operationName='" + operationName + '\'' +
+                    ", name='" + name + '\'' +
+                    ", id=" + id +
+                    ", mIds=" + mIds +
+                    "}\n";
+        }
+    }
+
+
+}
+
+
--- src/main/java/com/example/demo/SpringSessionApplication.java
+++ src/main/java/com/example/demo/SpringSessionApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.EtClient.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
@@ -23,8 +27,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,6 +42,11 @@ public class SpringSessionApplication {
     private static final Logger logger = LoggerFactory.getLogger(SpringSessionApplication.class);
 
     private static final String USER_INFO_URI = "https://%s.auth-qa1.marketingcloudqaapis.com/v2/userinfo";
+    private static final String ET_REST_TSSD =
+            "https://mc882zkqmv0jpg-dz8-ng69z7ws4.rest-qa1.marketingcloudqaapis.com/";
+
+    @Autowired
+    private EtClient etClient;
 
     @Bean
     public static ConfigureRedisAction configureRedisAction() {
@@ -49,11 +61,12 @@ public class SpringSessionApplication {
     public String email1(
             Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
             @AuthenticationPrincipal OAuth2User oAuth2User) {
+        final List<Item> permissions = etClient.permissions(authorizedClient.getAccessToken().getTokenValue());
         model.addAttribute("userName", oAuth2User.getName());
 
-        Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
-        attributes.put("TOKEN", authorizedClient.getAccessToken().getTokenValue());
-
+        final Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+        attributes.put("token", authorizedClient.getAccessToken().getTokenValue());
+        attributes.put("permissions", permissions);
         model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
         model.addAttribute("userAttributes", attributes);
         return "index";
@@ -85,6 +98,17 @@ public class SpringSessionApplication {
                 return delegate.loadUser(request);
             }
         };
+    }
+
+
+    @Bean
+    public RestTemplate template(RestTemplateBuilder builder) {
+        return builder.setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .rootUri(ET_REST_TSSD)
+                // use apache http client
+                .requestFactory(HttpComponentsClientHttpRequestFactory.class)
+                .build();
     }
 }
 
