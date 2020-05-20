46c4d61ddf8b0e70b75423c95d1e5520b3806c0e
salliEG <sallievergage@gmail.com>
2020-05-20 09:42:58 -0400

    making all final change. useing redis, ignoring tests

--- src/main/java/com/example/demo/SpringSessionApplication.java
+++ src/main/java/com/example/demo/SpringSessionApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,33 +17,50 @@ import org.springframework.security.oauth2.client.web.HttpSessionOAuth2Authorize
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.session.FlushMode;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @SpringBootApplication
-//@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
+@EnableRedisHttpSession(flushMode = FlushMode.IMMEDIATE)
 @Slf4j
-public class SpringSessionApplication{
+public class SpringSessionApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SpringSessionApplication.class);
 
     private static final String USER_INFO_URI = "https://%s.auth-qa1.marketingcloudqaapis.com/v2/userinfo";
-    private static final String REST_URL = "https://(\\S+).rest.marketingcloudapis.com";
 
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionApplication.class, args);
+    }
 
     @GetMapping("/")
-    public String email1(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                         @AuthenticationPrincipal OAuth2User oAuth2User) {
+    public String email1(
+            Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+            @AuthenticationPrincipal OAuth2User oAuth2User) {
         model.addAttribute("userName", oAuth2User.getName());
+
+        Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+        attributes.put("TOKEN", authorizedClient.getAccessToken().getTokenValue());
+
         model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-        model.addAttribute("userAttributes", oAuth2User.getAttributes());
+        model.addAttribute("userAttributes", attributes);
         return "index";
     }
 
     @Bean
-    public OAuth2AuthorizedClientRepository repository(){
+    public OAuth2AuthorizedClientRepository repository() {
         return new HttpSessionOAuth2AuthorizedClientRepository();
     }
 
@@ -49,26 +69,22 @@ public class SpringSessionApplication{
         return new OAuth2UserService<OAuth2UserRequest, OAuth2User>() {
 
             private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
             @Override
             public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
                 final String restUrl = userRequest.getAdditionalParameters().get("rest_instance_url").toString();
                 final String tssd = restUrl.substring(8, 8 + 28); // TSSD is 28 character length.
-                final ClientRegistration registration = ClientRegistration.withClientRegistration(userRequest.getClientRegistration())
-                                                                          .userInfoUri(String.format(USER_INFO_URI, tssd))
-                                                                          .build();
-                final OAuth2UserRequest request = new OAuth2UserRequest(registration, userRequest.getAccessToken(), userRequest.getAdditionalParameters());
+                final ClientRegistration registration =
+                        ClientRegistration.withClientRegistration(userRequest.getClientRegistration())
+                                .userInfoUri(String.format(USER_INFO_URI, tssd))
+                                .build();
+                logger.info("UserInfo route is {}", registration.getProviderDetails().getUserInfoEndpoint().getUri());
+                final OAuth2UserRequest request = new OAuth2UserRequest(registration,
+                                                                        userRequest.getAccessToken(),
+                                                                        userRequest.getAdditionalParameters());
                 return delegate.loadUser(request);
             }
         };
-    }
-
-//    @Bean
-//    public static ConfigureRedisAction configureRedisAction() {
-//        return ConfigureRedisAction.NO_OP;
-//    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringSessionApplication.class, args);
     }
 }
 
